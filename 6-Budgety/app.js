bce9749dac180f8b325c980d4ff94b75998ae8c1
Thiago Xanhenha <tjbarbosa@uolinc.com>
2017-03-07 11:36:02 -0300

    Section 6 - Lecture 77 - Budget Controller

--- 6-Budgety/app.js
+++ 6-Budgety/app.js
@@ -13,6 +13,14 @@ var budgetController = (function(){
 		this.value = value;
 	};
 
+	var calculateTotal = function(type) {
+		var sum = 0;
+		data.allItems[type].forEach(function(cur) {
+			sum += cur.value;
+		});
+		data.totals[type] = sum;
+	}
+
 	// variavel criada para guardar todos os valores cadastrados e os totais
 	var data = {
 		allItems: {
@@ -22,10 +30,13 @@ var budgetController = (function(){
 		totals: {
 			exp: 0,
 			inc: 0
-		}
+		},
+		budget: 0,
+		percentage: -1
 	};
 
 	return {
+
 		addItem: function(type, des, val) {
 			var newItem, ID;
 
@@ -49,6 +60,32 @@ var budgetController = (function(){
 			// Return a new element
 			return newItem;
 
+		},
+
+		calculateBudget: function() {
+
+			// calculate total income and expense
+			calculateTotal('exp');
+			calculateTotal('inc');
+
+			// calculate the budget: income - expenses
+			data.budget = data.totals.inc - data.totals.exp;
+
+			// calculate the percentage of income that we spent
+			if(data.totals.inc > 0) {
+				data.percentage = Math.round( (data.totals.exp / data.totals.inc) * 100 );
+			} else  {
+				data.percentage = -1;
+			}
+		},
+
+		getBudget: function() {
+			return {
+				budget: data.budget,
+				totalInc: data.totals.inc,
+				totalExp: data.totals.exp,
+				percentage: data.percentage
+			};
 		},
 
 		testing: function() {
@@ -141,8 +178,13 @@ var controller = (function(budgetCtrl,UICtrl) {
 
 	var updateBudget = function() {
 		// 1. Calculate budget
+		budgetCtrl.calculateBudget();
 
 		// 2. Return the budget
+		var budget = budgetCtrl.getBudget();
+
+		// 3. Display the budget on the UI
+		console.log(budget);
 	}
 
 	var ctrlAddItem = function() {
