2fb4d3c6f165d62cd3e415d78ee397e981b94018
Thiago Xanhenha <tjbarbosa@uolinc.com>
2017-03-14 10:25:23 -0300

    Section 6 - Lecture 86 - Updating the item percentages

--- 6-Budgety/app.js
+++ 6-Budgety/app.js
@@ -5,7 +5,20 @@ var budgetController = (function(){
 		this.id = id;
 		this.description = description;
 		this.value = value;
+		this.percentage = -1;
 	};
+
+	Expense.prototype.calcPercentage = function(totalIncome) {
+		if(totalIncome > 0) {
+			this.percentage = Math.round((this.value / totalIncome) * 100);
+		} else {
+			this.percentage = -1;
+		}
+	};
+
+	Expense.prototype.getPercentage = function() {
+		return this.percentage;
+	}
 
 	var Income = function(id, description, value) {
 		this.id = id;
@@ -95,6 +108,21 @@ var budgetController = (function(){
 			} else  {
 				data.percentage = -1;
 			}
+		},
+
+		calculatePercentages: function() {
+
+			data.allItems.exp.forEach(function(cur) {
+				cur.calcPercentage( data.totals.inc );
+			});
+
+		},
+
+		getPercentages: function() {
+			var allPerc = data.allItems.exp.map(function(cur) {
+				return cur.getPercentage();
+			});
+			return allPerc;
 		},
 
 		getBudget: function() {
@@ -231,6 +259,19 @@ var controller = (function(budgetCtrl,UICtrl) {
 		UICtrl.displayBudget(budget);
 	};
 
+	var updatePercentages = function() {
+
+		// 1. Calculate percentages
+		budgetCtrl.calculatePercentages();
+
+		// 2. Read percentages from budget controller
+		var percentages = budgetCtrl.getPercentages();
+
+		// 3. UPdate the UI with the new percentages
+		console.log(percentages);
+
+	};
+
 	var ctrlAddItem = function() {
 
 		var input, newItem;
@@ -250,6 +291,9 @@ var controller = (function(budgetCtrl,UICtrl) {
 
 			// 5. Calculate and update budget
 			updateBudget();
+
+			// 6. Calculate the percentages
+			updatePercentages();
 		}
 
 	};
@@ -275,6 +319,9 @@ var controller = (function(budgetCtrl,UICtrl) {
 
 			// 3. Update and sohw the new budget
 			updateBudget();
+
+			// 4. Calculate the percentages
+			updatePercentages();
 
 		}
 
