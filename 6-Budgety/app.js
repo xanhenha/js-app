5eb738127cc8a5b0902a1dda705999d18cbd63e0
Thiago Xanhenha <tjbarbosa@uolinc.com>
2017-03-22 09:05:58 -0300

    Section 6 - Lecture 90 - Improving the UX

--- 6-Budgety/app.js
+++ 6-Budgety/app.js
@@ -186,6 +186,12 @@ var UIController = (function(){
 
 	};
 
+	var nodeListForEach = function(list, callback) {
+		for(var i = 0; i < list.length; i++) {
+			callback(list[i], i);
+		}
+	};
+
 	return {
 		getinput: function() {
 			return {
@@ -257,12 +263,6 @@ var UIController = (function(){
 
 			var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
 
-			var nodeListForEach = function(list, callback) {
-				for(var i = 0; i < list.length; i++) {
-					callback(list[i], i);
-				}
-			}
-
 			nodeListForEach(fields, function(current, index) {
 
 				console.log(percentages[index]);
@@ -287,6 +287,20 @@ var UIController = (function(){
 			document.querySelector(DOMstrings.dateLabel).textContent = months[month] +' '+ year;
 		},
 
+		changedType: function() {
+			var fields = document.querySelectorAll(
+				DOMstrings.inputType + ',' +
+				DOMstrings.inputDescription + ',' +
+				DOMstrings.inputValue
+			);
+
+			nodeListForEach(fields, function(cur) {
+				cur.classList.toggle('red-focus');
+			});
+
+			document.querySelector(DOMstrings.inputButton).classList.toggle('red');
+		},
+
 		getDOMstrings: function() {
 			return DOMstrings;
 		}
@@ -309,7 +323,8 @@ var controller = (function(budgetCtrl,UICtrl) {
 			}
 		});
 
-		document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem)
+		document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+		document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType);
 	};
 
 	var updateBudget = function() {
