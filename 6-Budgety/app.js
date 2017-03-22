2c7e0f9138a2a199079b22a893d8661079ed3342
Thiago Xanhenha <tjbarbosa@uolinc.com>
2017-03-22 08:51:24 -0300

    Section 6 - Lecture 89 - Displaying the Current Month and Year

--- 6-Budgety/app.js
+++ 6-Budgety/app.js
@@ -157,7 +157,8 @@ var UIController = (function(){
 		expensesLabel: '.budget__expenses--value',
 		percentageLabel: '.budget__expenses--percentage',
 		container: '.container',
-		expensesPercLabel: '.item__percentage'
+		expensesPercLabel: '.item__percentage',
+		dateLabel: '.budget__title--month'
 	}
 
 	var formatNumber = function(num, type) {
@@ -275,6 +276,17 @@ var UIController = (function(){
 
 		},
 
+		displayMonth: function(){
+			var now, year, month, months;
+			now = new Date();
+
+			months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+			month = now.getMonth();
+			year = now.getFullYear();
+			document.querySelector(DOMstrings.dateLabel).textContent = months[month] +' '+ year;
+		},
+
 		getDOMstrings: function() {
 			return DOMstrings;
 		}
@@ -381,7 +393,8 @@ var controller = (function(budgetCtrl,UICtrl) {
 
 	return {
 		init: function() {
-			console.log('Aplication has started.')
+			console.log('Aplication has started.');
+			UICtrl.displayMonth();
 			UICtrl.displayBudget({
 				budget: 0,
 				totalInc: 0,
