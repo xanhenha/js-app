69ddc5b7663e1276933e2d3cd5c32982700728cd
Thiago Xanhenha <tjbarbosa@uolinc.com>
2017-03-30 13:09:50 -0300

    Section 7 - ES6 - Arrow Functions Basics

--- 7-EcmaScript2015/script.js
+++ 7-EcmaScript2015/script.js
@@ -84,7 +84,7 @@ console.log(i);
 
 //////////////////
 // Lecture: Strings
-
+/*
 let firstName = 'Jonh';
 let lastName = 'Smith';
 const yearOfBirth = 1990;
@@ -104,3 +104,29 @@ console.log(n.startsWith('j'));
 console.log(n.endsWith('th'));
 console.log(n.includes(' '));
 console.log(`${firstName} `.repeat(5));
+*/
+
+//////////////////
+// Lecture: Arrow functions
+
+const years = [1990, 1965, 1982, 1937];
+
+// ES5
+var ages5 = years.map(function(el){
+    return 2017 - el;
+});
+console.log(ages5);
+
+// ES6
+let ages6 = years.map(el => 2016 - el);
+console.log(ages6);
+
+ages6 = years.map((el, index) => `Age element ${index + 1}: ${2016 - el}.`)
+console.log(ages6);
+
+ages6 = years.map((el, index) =>  {
+    const now = new Date().getFullYear();
+    const age = now - el;
+    return `Age element ${index + 1}: ${2016 - el}.`;
+});
+console.log(ages6);
